951710b02df3f4309a4d2ad4184e454e2be6ba06
Patrice ALLARY <dev@allary-guillery.fr>
2023-10-23 17:05:44 +0200

    First Commit

---
+++ src/main/java/info/patriceallary/myapplicationsboard/MyApplicationsBoardApplication.java
@@ -0,0 +1,13 @@
+package info.patriceallary.myapplicationsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplicationsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplicationsBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/patriceallary/myapplicationsboard/MyApplicationsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package info.patriceallary.myapplicationsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
