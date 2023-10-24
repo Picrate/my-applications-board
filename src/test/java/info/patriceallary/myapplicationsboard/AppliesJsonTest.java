63af0700dad251e8780b9c165a2043a00a560416
Patrice ALLARY <dev@allary-guillery.fr>
2023-10-24 18:01:36 +0200

    create Apply Serialization/ Deserialization Tests

--- src/test/java/info/patriceallary/myapplicationsboard/AppliesJsonTest.java
+++ src/test/java/info/patriceallary/myapplicationsboard/AppliesJsonTest.java
@@ -1,5 +1,8 @@
 package info.patriceallary.myapplicationsboard;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 @JsonTest
-public class JobJsonTest {
+public class AppliesJsonTest {
 
 
     @Autowired
@@ -21,27 +24,52 @@ public class JobJsonTest {
     private JacksonTester<Apply[]> jsonList;
     private Apply[] applies;
 
-    private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     @BeforeEach
     void setup() throws ParseException {
+
+
+
         applies = Arrays.array(
-                new Apply(1L, "My-First-Apply", formatter.parse("12/10/2023")),
-                new Apply(2L, "My-Second-Apply", formatter.parse("13/10/2023")),
-                new Apply(3L, "My-Third-Apply", formatter.parse("14/10/2023"))
+                new Apply(1L, "My-First-Apply", LocalDateTime.parse("2023-10-12T00:00:00.000+00:00", formatter)),
+                new Apply(2L, "My-Second-Apply", LocalDateTime.parse("2023-10-13T23:59:00.000+00:00", formatter)),
+                new Apply(3L, "My-Third-Apply", LocalDateTime.parse("2023-10-14T12:01:00.000+00:00", formatter))
         );
     }
 
     @Test
-    public void JobSerializationTest() throws ParseException, IOException {
+    public void applySerializationTest() throws ParseException, IOException {
 
-        Apply apply = new Apply(1L, "My-First-Apply", formatter.parse("12/10/2023"));
-
+        Apply apply = new Apply(1L, "My-First-Apply", LocalDateTime.parse("2023-10-13T23:59:00", formatter));
         // Check if apply object is esqual to json object store in "expected.json" file
         assertThat(json.write(apply)).isStrictlyEqualToJson("expected.json");
+        // Check json has id parameter and is esqual to 1
+        assertThat(json.write(apply)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(apply)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+        // Check if json has "name paramter and is equal to "My-First-Apply" ignoring case
+        assertThat(json.write(apply)).hasJsonPathStringValue("@.name");
+        assertThat(json.write(apply)).extractingJsonPathStringValue("@.name").isEqualToIgnoringCase("My-First-Apply");
+        // Check Json has dateCreated paramter with "2023-10-13T23:59:00" Value
+        assertThat(json.write(apply)).hasJsonPathStringValue("@.dateCreated");
+        assertThat(json.write(apply)).extractingJsonPathStringValue("@.dateCreated").isEqualToIgnoringWhitespace(apply.dateCreated().format(formatter));
+    }
 
+    @Test
+    public void applyDeserializationTest() throws IOException {
+        String expected = """
+                {
+                  "id": 1,
+                  "name": "My-First-Apply",
+                  "dateCreated" : "2023-10-13T23:59:00"
+                }
+                """;
+        Apply testApply = new Apply(1L, "My-First-Apply", LocalDateTime.parse("2023-10-13T23:59:00", formatter));
 
-
+        assertThat(json.parseObject(expected)).isEqualTo(testApply);
+        assertThat(json.parseObject(expected).id()).isEqualTo(1);
+        assertThat(json.parseObject(expected).name()).isEqualTo("My-First-Apply");
+        assertThat(json.parseObject(expected).dateCreated()).isEqualTo(testApply.dateCreated().format(formatter));
     }
 
 
