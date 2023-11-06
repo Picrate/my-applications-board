3f80a8a500d7c7030141175f264e9c95db9e7246
Patrice ALLARY <dev@allary-guillery.fr>
2023-11-06 17:23:36 +0100

    Correct bug sql script data initialization

--- src/main/java/info/patriceallary/myapplicationsboard/PersistenceConfig.java
+++ src/main/java/info/patriceallary/myapplicationsboard/PersistenceConfig.java
@@ -1,7 +1,5 @@
 package info.patriceallary.myapplicationsboard;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -31,15 +31,14 @@ insert into ADDRESS (ADDRESS1, ADDRESS2, ZIP_CODE, CITY) values ('0452 Summer Ri
 insert into ADDRESS (ADDRESS1, ADDRESS2, ZIP_CODE, CITY) values ('24 Morrow Circle', 'Apt 1601', null, 'Weetobula');
 insert into ADDRESS (ADDRESS1, ADDRESS2, ZIP_CODE, CITY) values ('64374 Farwell Court', 'Suite 20', null, 'Daojiang');
 
-INSERT INTO CONTACTS(FIRSTNAME, LASTNAME, EMAIL, ADDRESS_ID, ROLE_ID, TITLE_ID) VALUES
-    ('Nanine', 'Pea', 'npea0@ifeng.com', 1,1,2 ),
-    ('Sandie', 'Thorbon', 'sthorbon1@wufoo.com', 2,2,2),
-    ('Aleta', 'Bradbeer', 'abradbeer2@springer.com', 3,3,1)
-;
-
 insert into ENTERPRISE (NAME, PHONE, NOTES, ADDRESS_ID, TYPE_ID, ACTIVITY_ID) values ('Yadel', '6672553534', 'quis orci eget orci vehicula', 3, 1, 1);
 insert into ENTERPRISE (NAME, PHONE, NOTES, ADDRESS_ID, TYPE_ID, ACTIVITY_ID) values ('Flashdog', '2525243945', 'odio condimentum id luctus nec molestie', 1, 3, 2);
 insert into ENTERPRISE (NAME, PHONE, NOTES, ADDRESS_ID, TYPE_ID, ACTIVITY_ID) values ('Feedfire', '3001738561', 'ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia', 2, 2, 3);
+
+INSERT INTO CONTACTS(FIRSTNAME, LASTNAME, EMAIL, ADDRESS_ID, ROLE_ID, TITLE_ID, ENTERPRISE_ID) VALUES
+                                                                                    ('Nanine', 'Pea', 'npea0@ifeng.com', 1,1,2,1 ),
+                                                                                    ('Sandie', 'Thorbon', 'sthorbon1@wufoo.com', 2,2,2,2),
+                                                                                    ('Aleta', 'Bradbeer', 'abradbeer2@springer.com', 3,3,1,3);
 
 insert into JOB (NAME, DATE_CREATED, STATUS_ID, ENTERPRISE_ID) values ('My-First-Apply', '2023-10-12 00:00:00.000', 2, 3);
 insert into JOB (NAME, DATE_CREATED, STATUS_ID, ENTERPRISE_ID) values ('My-Second-Apply', '2023-10-13 23:59:00.000', 1, 3);
