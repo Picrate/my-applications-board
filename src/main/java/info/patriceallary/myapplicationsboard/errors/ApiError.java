ffed1e4531ce739490fc1a7546c1714248c9035b
Patrice ALLARY <dev@allary-guillery.fr>
2023-11-08 14:54:07 +0100

    Implémentation Exception Handlers

--- src/main/java/info/patriceallary/myapplicationsboard/errors/ApiError.java
+++ src/main/java/info/patriceallary/myapplicationsboard/errors/ApiError.java
@@ -1,2 +1,39 @@
-package info.patriceallary.myapplicationsboard.errors;public class ApiError {
+package info.patriceallary.myapplicationsboard.errors;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus httpStatus, String message, List<String> errors) {
+        super();
+        this.status = httpStatus;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus httpStatus, String message, String error) {
+        super();
+        this.status = httpStatus;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
 }
