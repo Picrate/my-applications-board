a02b69fb38a5ff77310b72c491510eef56800eed
Patrice ALLARY <dev@allary-guillery.fr>
2023-11-13 16:38:48 +0100

    Add SQL Datas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO JOB_RESULTS(NAME) VALUES
+                                  ( 'Acceptée' ),
+                                  ('Refusée par l employeur'),
+                                  ('Refusée')
+;
+
+INSERT INTO CONTACT_TITLE(NAME) VALUES
+                                   ( 'Mr' ),
+                                   ( 'Mme' )
+;
+
+INSERT INTO CONTACT_ROLES(NAME) VALUES
+                                    ( 'Chargé de recrutement IT' ),
+                                    ( 'Responsable des Ressources Humaines' ),
+                                    ( 'Dirigeant' )
+;
+
+INSERT INTO ENTERPRISE_TYPE(NAME) VALUES
+                                      ( 'PME' ),
+                                      ( 'Grand Groupe' ),
+                                      ( 'ETI' )
+;
+
+INSERT INTO ENTERPRISE_ACTIVITY(NAME) VALUES
+                                          ( 'Assurance' ),
+                                          ( 'Banque' ),
+                                          ( 'ESN' )
+;
